37ba7252ef37a56100bc3e280647cc43dc22a778
Bishop Hall <bishop.e.hall@gmail.com>
2021-12-18 00:58:22 -0500

    fibonachi

---
+++ fibonacci2.py
@@ -0,0 +1,12 @@
+
+def fib(n):
+    if (n == 1):
+        return 1
+    if (n == 0):
+        return 0
+    return fib(n-1) + fib(n-2)
+
+
+
+for i in range(100):
+  print(str(i) + " : " + str(fib(i)))
--- fibonachiseqence.py
+++ fibonachiseqence.py
@@ -1,11 +1,23 @@
-count_to = int(input("what number in the Fibonacci sequence do you want to go up to for example : 10 will count up to 55."))
-count = 2
-num1 = 1
-num2 = 1
-while(count < count_to):
-    current_num = num1 + num2
-    num1 = num2
-    num2 = current_num
-    count += 1
+# count_to = int(input("what number in the Fibonacci sequence do you want to go up to for example : 10 will count up to 55."))
+current_num = 0
 
-print(current_num)
+
+def fib(n):
+    if (n == 0):
+        return 0
+    if (n == 1):
+        return 1
+    if (n == 2):
+        return 1
+    count = 0
+    num1 = 0
+    num2 = 1
+
+    while (count < n):
+        current_num = num1 + num2
+        num1 = num2
+        num2 = current_num
+        count += 1
+    return current_num
+for i in range(110):
+    print(str(i) + " : " + str(fib(i)))
